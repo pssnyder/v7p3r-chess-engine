5db7d049ec9f68f363dde37d4ff1603a90575d76
Patrick Snyder <2072916+pssnyder@users.noreply.github.com>
2025-07-24 22:53:31 -0400

    additional cleanup of beta-candidate files

--- builds_complete/v0.6.27_beta-candidate-3_COMPLETE/engine_utilities/v7p3r_config_gui.app.py
+++
@@ -1,2 +0,0 @@
-﻿# enginge_utilities/v7p3r_gui.app.py
-# TODO: A webapp that allows configuration and visual management of the Viper Chess Engine settings via a simple GUI
--- builds_complete/v0.6.9_beta-candidate-11_COMPLETE/engine_utilities/viper_gui.app.py
+++
@@ -1,2 +0,0 @@
-﻿# enginge_utilities/viper_gui.app.py
-# TODO: A webapp that allows configuration and visual management of the Viper Chess Engine settings via a simple GUI
